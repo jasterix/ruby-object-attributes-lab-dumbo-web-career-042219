cfdda5616453e6af2078b41a196301a8d3f6a46f
Jasterix C <loune.calixte@gmail.com>
2019-04-24 03:42:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,9 +5,9 @@ class Dog
   def name 
     @name
   end
-  def name=(new_name)
-    @name =  new_name
-  end
+  # def name=(new_name)
+  #   @name =  new_name
+  # end
     
   def breed=(breed)
     @breed = breed
